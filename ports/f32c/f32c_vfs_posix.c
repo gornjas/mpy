3df3e02fd97d826819f2b3eaf94bdf100a9c7d30
gornjas <zec@fer.hr>
2024-11-10 14:44:37 +0100

    ciscenje vfs wrappera, neki su preseljeni u f32c libc...

--- ports/f32c/f32c_vfs_posix.c
+++ ports/f32c/f32c_vfs_posix.c
@@ -2,16 +2,6 @@
 
 #include <fatfs/ff.h>
 
-int
-chdir(const char *path) {
-	return f_chdir(path);
-}
-
-char *
-getcwd(char *buf, size_t size) {
-	f_getcwd(buf, size);
-	return buf;
-};
 
 DIR *
 opendir(const char *path) {
@@ -32,30 +22,18 @@ readdir(DIR *dirp) {
 
 int
 closedir(DIR *dirp) {
+
 	return f_closedir(dirp);
 };
 
 int
-mkdir(const char *path, mode_t mode) {
-	return f_mkdir(path);
-};
-
-int
-rmdir(const char *path) {
-	return f_rmdir(path);
-};
-
-int
-rename(const char *from, const char *to) {
-	return f_rename(from, to);
-};
-
-int
 fsync(int fd) {
+
 	return 0; /* XXX FIXME! */
 };
 
 int
 statvfs(const char * restrict path, struct statvfs * restrict buf) {
+
 	return 0; /* XXX FIXME! */
 };
