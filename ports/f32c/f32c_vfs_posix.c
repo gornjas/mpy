e040db19ffc1bc1126f2022cc8cdb33050dd9885
gornjas <zec@fer.hr>
2024-11-11 22:25:40 +0100

    ciscenje funkcija koje su preseljene u f32c libc

--- ports/f32c/f32c_vfs_posix.c
+++ ports/f32c/f32c_vfs_posix.c
@@ -1,36 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include <sys/statvfs.h>
 
 #include <fatfs/ff.h>
 
-
-DIR *
-opendir(const char *path) {
-	DIR *dp = NULL;
-
-	f_opendir(dp, path);
-	return dp;
-};
-
-struct dirent *
-readdir(DIR *dirp) {
-        FILINFO fno;
-	struct dirent *de = NULL;
-
-	f_readdir(dirp, &fno);
-	return de; /* XXX FIXME */
-};
-
-int
-closedir(DIR *dirp) {
-
-	return f_closedir(dirp);
-};
 
 int
 fsync(int fd) {
 
 	return 0; /* XXX FIXME! */
 };
+
 
 int
 statvfs(const char * restrict path, struct statvfs * restrict buf) {
