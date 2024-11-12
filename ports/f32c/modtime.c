a3ed541763ee18067490ed97db9d8c71d93fba6c
gornjas <zec@fer.hr>
2024-11-12 07:00:19 +0100

    modtime cleanup

--- ports/f32c/modtime.c
+++ ports/f32c/modtime.c
@@ -1,52 +1,12 @@
-/*
- * This file is part of the MicroPython project, http://micropython.org/
- *
- * The MIT License (MIT)
- *
- * Copyright (c) 2014-2017 Paul Sokolovsky
- * Copyright (c) 2014-2023 Damien P. George
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-#include <unistd.h>
-#include <errno.h>
-#include <stdio.h>
-#include <string.h>
 #include <time.h>
-#include <sys/time.h>
-#include <math.h>
 
 #include "py/mphal.h"
 #include "py/runtime.h"
-
-#if 0
-STATIC mp_obj_t mp_time_sleep(mp_obj_t arg) {
-    return mp_const_none;
-}
-#endif
 
 #if MICROPY_PY_TIME_TIME_TIME_NS
 static mp_obj_t mp_time_time_get(void) {
     struct timespec tv;
 
-    //clock_gettime(CLOCK_REALTIME, &tv);
     clock_gettime(CLOCK_MONOTONIC, &tv);
 
     mp_float_t val = tv.tv_sec + (mp_float_t)tv.tv_nsec / 1000000000;
